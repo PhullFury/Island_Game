236948bfcb15816e957101a2af7b48888a5ea049
PhullFury <66832169+PhullFury@users.noreply.github.com>
2023-05-08 19:09:12 -0600

    Restarting this project

--- Source/Island_Game/Characters/IslandMan.cpp
+++ Source/Island_Game/Characters/IslandMan.cpp
@@ -1,7 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Characters/IslandMan.h"
+#include "IslandMan.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 // Sets default values
 AIslandMan::AIslandMan()
@@ -9,6 +12,12 @@ AIslandMan::AIslandMan()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	GetCapsuleComponent()->InitCapsuleSize(34.f, 60.f);
+
+	/*FPCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("First Person Camera"));
+	FPCamera->SetupAttachment(GetCapsuleComponent());
+	FPCamera->SetRelativeLocation(FVector(0.f, 0.f, 40.f));
+	FPCamera->bUsePawnControlRotation = true;*/
 }
 
 // Called when the game starts or when spawned
@@ -16,6 +25,7 @@ void AIslandMan::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	GetCharacterMovement()->MaxWalkSpeed = IslandManSpeed;
 }
 
 // Called every frame
@@ -29,6 +39,29 @@ void AIslandMan::Tick(float DeltaTime)
 void AIslandMan::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+	PlayerInputComponent->BindAxis(TEXT("Forward"), this, &AIslandMan::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("Sideways"), this, &AIslandMan::MoveSideways);
+	PlayerInputComponent->BindAxis(TEXT("LookVertical"), this, &AIslandMan::LookVertical);
+	PlayerInputComponent->BindAxis(TEXT("LookHorizontal"), this, &AIslandMan::LookHorizontal);
+	PlayerInputComponent->BindAction(TEXT("Jump"), IE_Pressed, this, &ACharacter::Jump);
 }
 
+void AIslandMan::MoveForward(float AxisValue)
+{
+	AddMovementInput(GetActorForwardVector() * AxisValue);
+}
+
+void AIslandMan::MoveSideways(float AxisValue)
+{
+	AddMovementInput(GetActorRightVector() * AxisValue);
+}
+
+void AIslandMan::LookVertical(float AxisValue)
+{
+	AddControllerPitchInput(AxisValue * TurnRate * GetWorld()->GetDeltaSeconds());
+}
+
+void AIslandMan::LookHorizontal(float AxisValue)
+{
+	AddControllerYawInput(AxisValue * TurnRate * GetWorld()->GetDeltaSeconds());
+}
--- Source/Island_Game/Characters/IslandMan.h
+++ Source/Island_Game/Characters/IslandMan.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Character.h"
 #include "IslandMan.generated.h"
 
+class UCameraComponent;
+
 UCLASS()
 class ISLAND_GAME_API AIslandMan : public ACharacter
 {
@@ -14,16 +16,27 @@ class ISLAND_GAME_API AIslandMan : public ACharacter
 public:
 	// Sets default values for this character's properties
 	AIslandMan();
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+private:
+	/*UPROPERTY(VisibleAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+		UCameraComponent* FPCamera;*/
+	UPROPERTY(EditAnywhere, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+		float TurnRate = 10.f;
+	UPROPERTY(EditAnywhere, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+		float IslandManSpeed = 300.f;
 
+	void MoveForward(float AxisValue);
+	void MoveSideways(float AxisValue);
+	void LookVertical(float AxisValue);
+	void LookHorizontal(float AxisValue);
 };
